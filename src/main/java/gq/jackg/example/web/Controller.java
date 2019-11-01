c8a6d41927a5797a4545f29bc195ccd2da422611
JaeGi Jung <54255834+monoyaya@users.noreply.github.com>
2019-11-01 15:57:50 +0900

    .

--- src/main/java/gq/jackg/example/web/Controller.java
+++ src/main/java/gq/jackg/example/web/Controller.java
@@ -1,8 +1,5 @@
 package gq.jackg.example.web;
 
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,10 +11,5 @@ public class Controller {
 	@RequestMapping("/")
 	public ModelAndView index() {
 		return new ModelAndView("index.html");
-	}
-	
-	@GetMapping("/{name}")
-	public String hello(@PathVariable(name = "name", required = false) String name) {
-		return String.format(temp, StringUtils.isEmpty(name) ? "World" : name);
 	}
 }
